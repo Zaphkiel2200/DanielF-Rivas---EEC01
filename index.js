feb5da10feb876aba74c81e75209f6e2613579b0
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-03-06 10:13:27 -0500

    index.js

--- index.js
+++ index.js
@@ -2,4 +2,8 @@ const _ = require('lodash');
 
 const numeros = [1, 2, 3, 4, 5];
 const numerosInvertidos = _.reverse(numeros);
-console.log(numerosInvertidos); // [5, 4, 3, 2, 1]
+console.log(numerosInvertidos); 
+
+const valores = [0, "Hola", false, 42, "", null, "Mundo", undefined, NaN, true];
+const valoresFiltrados = _.compact(valores);
+console.log(valoresFiltrados); 
