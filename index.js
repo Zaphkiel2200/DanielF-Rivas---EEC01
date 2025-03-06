a3be9ca136cf366c0a1752c48a49889c4b00a228
Daniel <135277854+Zaphkiel2200@users.noreply.github.com>
2025-03-06 12:00:41 -0500

    desafio 6

--- index.js
+++ index.js
@@ -11,4 +11,21 @@ console.log(valoresFiltrados);
 const array1 = [1, 2, 3];
 const array2 = [3, 4, 5];
 const union = _.union(array1, array2);
-console.log(union); // [1, 2, 3, 4, 5]
+console.log(union); 
+
+const palabras = ["hola", "mundo", "hola", "javascript", "mundo", "hola"];
+const frecuencia = _.countBy(palabras);
+console.log(frecuencia);
+
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = [3, 4, 5, 6, 7];
+const diferencia = _.difference(arr1, arr2);
+console.log(diferencia);
+
+const personas = [
+    { nombre: "Ana", edad: 25 },
+    { nombre: "Luis", edad: 22 },
+    { nombre: "Juan", edad: 30 }
+  ];
+  const ordenados = _.sortBy(personas, 'edad');
+  console.log(ordenados);
